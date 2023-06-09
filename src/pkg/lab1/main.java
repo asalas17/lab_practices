133dd5a49d140c56814e66f8a85acc549219014c
asalas17 <andresalas.o17@gmail.com>
2023-06-09 15:10:02 -0600

    Practicas de lab inicializado

---
+++ src/pkg/lab1/main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkg.lab1;
+
+/**
+ *
+ * @author Andres
+ */
+public class main {
+    public static void main(String[] args) {
+        
+    }
+}
