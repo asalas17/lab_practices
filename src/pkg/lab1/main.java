98cfed476ec7afcdacc331e7d082e114f0476bf5
asalas17 <andresalas.o17@gmail.com>
2023-06-11 10:06:03 -0600

    Andres - 2 prints

--- src/pkg/lab1/main.java
+++ src/pkg/lab1/main.java
@@ -10,6 +10,8 @@ package pkg.lab1;
  */
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hola");
+        
+        System.out.println("");
+        System.out.println("");
     }
 }
